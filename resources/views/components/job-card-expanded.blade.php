24ec4f378176245d709c358d42e2b8870ade7aed
John Michael Isaac <johnmichaelisaac01@gmail.com>
2025-08-09 19:44:10 +0800

    Added more components for the UI.

---
+++ resources/views/components/job-card-expanded.blade.php
@@ -0,0 +1,16 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo />
+    </div>
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-600">Employer Name</a>
+
+        <h3 class="font-bold text-lg group-hover:text-blue-600 text-xl font-bold transition-colors duration-500">Job Title</h3>
+        <p class="text-sm text-gray-600 mt-auto">Salary per Year(USD)</p>
+    </div>
+    <div>
+        <x-tag size="small">Tag</x-tag>
+        <x-tag size="small">Tag</x-tag>
+        <x-tag size="small">Tag</x-tag>
+    </div>
+</x-panel>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,12 @@
 <x-layout>
     <div class="space-y-10">
-        <section>
+        <section class="text-center pt-6">
+            <h1 class="font-bold text-4xl">Let's Find You a J*b</h1>
+            <form action="#" class="mt-6">
+                <input type="text" placeholder="I'm looking for..." class="rounded-xl bg-white/15 border border-white/10 px-5 py-4 w-full max-w-xl">
+            </form>
+        </section>
+        <section class="pt-10">
             <x-section-heading class="mb-6">Featured Jobs</x-section-heading>
             <div class="grid lg:grid-cols-3 gap-8">
                 <x-job-card />
@@ -11,20 +17,25 @@
         <section>
             <x-section-heading>Tags</x-section-heading>
             <div class="mt-6 space-x-1">
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
+                <x-tag size="base">Tag</x-tag>
+                <x-tag size="base">Tag</x-tag>
+                <x-tag size="base">Tag</x-tag>
+                <x-tag size="base">Tag</x-tag>
+                <x-tag size="base">Tag</x-tag>
+                <x-tag size="base">Tag</x-tag>
+                <x-tag size="base">Tag</x-tag>
+                <x-tag size="base">Tag</x-tag>
+                <x-tag size="base">Tag</x-tag>
+                <x-tag size="base">Tag</x-tag>
             </div>
         </section>
         <section>
             <x-section-heading>Recent Jobs</x-section-heading>
+            <div class="mt-6 space-y-6">
+                <x-job-card-expanded />
+                <x-job-card-expanded />
+                <x-job-card-expanded />
+            </div>
         </section>
     </div>
 </x-layout>
